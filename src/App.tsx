21907ab834fae0a9cfbc5e66f53a3608f6a3e98b
zjoseph03 <zachjoseph@hotmail.com>
2026-03-09 22:41:38 -0700

    Added admin control panel on different link to show for demo day

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import QuoteRequest from "./pages/QuoteRequest";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import NotFound from "./pages/NotFound";
+import AdminDemo from "./pages/AdminDemo";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ const App = () => (
           <Route path="/quote-request" element={<QuoteRequest />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/admin/demo" element={<AdminDemo />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
